8bb87c2effa2c8ad6c5674333c59eb29de45447c
JacobArndt <jacobwarndt@gmail.com>
2025-04-28 14:28:35 -0400

    Refactor server initialization to improve clarity and maintainability

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const server = require('./app');
+const app = require('./app');
 
-server.listen(3000, function() {
-  console.log('Server is listening on http://localhost:3000');
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
